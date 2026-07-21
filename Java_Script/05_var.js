b68abad8814b10a0fd608776b3b2be9ebc0772e7
Aabhaykumarr <aabhaykumarr9199@gmail.com>
2026-07-21 17:07:40 +0530

    learned basics of js

---
+++ Java_Script/05_var.js
@@ -0,0 +1,9 @@
+let variable1 = 3;
+console.log(variable1);
+let calculation = 2+2;
+
+console.log(calculation);
+console.log(calculation  +2 );
+// the logic are same as any other programming language such as c++;
+let a = (2+2);
+console.log(`the price is ${a}`);
--- Java_Script/practice.js
+++ Java_Script/practice.js
@@ -1,1 +1,2 @@
 console.log("wake up to reality");
+console.log(2+2);
